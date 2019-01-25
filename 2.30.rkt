460b2667702e54f39d30e6a257a78d762e6b94f4
roman <roman.ushakov.89@inbox.ru>
2019-01-25 10:47:54 +0300

    add 2.30

---
+++ 2.30.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require rackunit)
+(require "./helpers.rkt")
+
+(define (square-tree tree)
+  (define (iter acc tree)
+    (if (null? tree)
+        acc
+        (iter
+         (append acc
+                 (if (not (list? (car tree)))
+                     (list (square (car tree)))
+                     (list (square-tree (car tree)))))
+         (cdr tree))))
+  (iter (list) tree))
+    
+
+(check-equal?
+ (square-tree (list 1
+                    (list 2 (list 3 4) 5)
+                    (list 6 7)))
+ (list 1 (list 4 (list 9 16) 25) (list 36 49)))
