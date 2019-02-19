90f6b79b4fc288eba840b83f034aaf833694c596
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-02-19 07:50:54 +0300

    add 3.14

--- 3.2.rkt
+++ 3.2.rkt
@@ -1,1 +1,17 @@
 #lang racket
+
+(require rackunit)
+
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (m)
+      (if (eq? m `how-many-calls)
+          count
+          (begin (set! count (add1 count)) (f m))))))
+
+(define s (make-monitored sqrt))
+
+(check-equal? (s 4) 2)
+(check-equal? (s 9) 3)
+
+(check-equal? (s `how-many-calls) 2)
