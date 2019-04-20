06913930a32fedc550a527cdaf68821a45d3acb5
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-04-20 14:06:19 +0300

    add 1.16

---
+++ 1.16.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require rackunit)
+
+(define (square x) (* x x))
+
+(define (fast-expt b n)
+  (define (iter b n a)
+    (cond
+      ((= n 0) a)
+      ((even? n) (iter (square b) (/ n 2) a))
+      (else (iter b (- n 1) (* a b)))
+    )
+  )
+
+  (iter b n 1)
+)
+
+(check-equal? (fast-expt 2 5) 32)
+(check-equal? (fast-expt 2 6) 64)
