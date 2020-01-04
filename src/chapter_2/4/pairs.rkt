771fd06a72829ecded2dcc84fc83e23e5c9f0c66
roman_ushakov <roman.ushakov.89@inbox.ru>
2020-01-04 21:23:27 +0300

    feat: add 2.4

---
+++ src/chapter_2/4/pairs.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+
+(provide (all-defined-out))
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (x y) x)))
+
+(define (cdr z)
+  (z (lambda (x y) y)))
