90a1be25a2ead49d7beb09d4cfcac1dccf2cd769
roman_ushakov <roman.ushakov.89@inbox.ru>
2020-05-29 18:08:36 +0300

    feat: add 2.36

---
+++ src/chapter_2/36/__tests__/accumulate-n-test.rkt
@@ -0,0 +1,6 @@
+#lang racket
+(require rackunit "../accumulate-n.rkt")
+
+(test-case "should return accumulated sequences"
+  (define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9 ) (list 10 11 12)))
+  (check-equal? (accumulate-n + 0 s) (list 22 26 30)))
---
+++ src/chapter_2/36/accumulate-n.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(require "../33/base.rkt")
+
+(provide (all-defined-out))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      null
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
